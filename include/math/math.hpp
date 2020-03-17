a7ddde7ccd63b10fa6cb12fb96b1b660ff8bab03
asunan9sha <asunan9sha@gmail.com>
2020-03-18 00:23:03 +0200

    created math header

---
+++ include/math/math.hpp
@@ -0,0 +1,23 @@
+//
+// Created by asunan9sha on 3/17/2020.
+//
+
+#ifndef ENGINE_MATH_HPP
+#define ENGINE_MATH_HPP
+
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+namespace eng {
+  using vec2 = glm::vec2;
+  using vec3 = glm::vec3;
+  using vec4 = glm::vec4;
+  using mat4 = glm::mat4;
+  using quat = glm::quat;
+}
+
+#endif //ENGINE_MATH_HPP
